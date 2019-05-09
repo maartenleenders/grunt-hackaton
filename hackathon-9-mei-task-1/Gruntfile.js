b6970c1f6e1597404bbe19dfc6bd0d0d68b0345b
Maarten Leenders <maarten@yoast.com>
2019-05-09 12:40:23 +0200

    Minimal version

--- hackathon-9-mei-task-1/Gruntfile.js
+++ hackathon-9-mei-task-1/Gruntfile.js
@@ -5,9 +5,37 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      assets: {
+        cwd: "<%= files.sourceFiles.path %>",
+        src: "<%= files.sourceFiles.mask %>",
+        dest: "<%= files.assets.path %>",
+        expand: true,
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          "assets/style.css": "assets/style.scss",
+        },
+      },
+    },
+    files: {
+      sourceFiles: {
+        path: 'source-files/',
+        mask: '*',
+      },
+      assets: {
+        path: 'assets/',
+        mask: '*',
+      },
+    }
   });
-
   // Load tasks here.
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Define aliases here.
   grunt.registerTask('default', 'My default task description', function() {
